e677d4eaad5164befa5dbf3ac83af9947d101aa8
Richard Attermeyer <richard.attermeyer@opitz-consulting.com>
2024-02-14 16:02:00 +0100

    Initial commit

---
+++ src/main/java/com/opitzconsulting/cattlecrew/jooqmigration/JooqMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.opitzconsulting.cattlecrew.jooqmigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqMigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opitzconsulting/cattlecrew/jooqmigration/JooqMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opitzconsulting.cattlecrew.jooqmigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
